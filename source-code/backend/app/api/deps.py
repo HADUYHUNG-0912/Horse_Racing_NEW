01b4ba524ca31373aba50e61e7720f8061e7993d
PhoDafan <nhuy22174@gmail.com>
2026-06-07 11:45:02 +0700

    update branch

--- source-code/backend/app/api/deps.py
+++ source-code/backend/app/api/deps.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.core.database import get_db
-from app.core.security import pwd_context
 from app.models.database_models import User, Role
 from app.schemas.auth import TokenPayload
 
